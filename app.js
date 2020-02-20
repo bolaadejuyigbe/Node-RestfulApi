2b50b11b7e63b78879a6eb6df22446767eda783b
adebola adejuyigbe <bolaadejuyigbe@yahoo.com>
2020-02-20 14:13:27 +0100

    adding user sign up

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 
  const productRoutes = require('./api/routes/products');
  const orderRoutes = require('./api/routes/orders');
+ const userRoutes = require('./api/routes/users');
 
  app.use(morgan('dev'));
  app.use('/uploads', express.static('uploads'));
@@ -30,8 +31,11 @@ const mongoose = require('mongoose')
 
  app.use('/products', productRoutes);
  app.use('/orders', orderRoutes);
+ app.use('/users', userRoutes);
+
+
  mongoose.connect('mongodb+srv://node-shop:' + process.env.MONGO_ATLAS_PW + '@node-rest-dkvm7.mongodb.net/test?retryWrites=true&w=majority',
- { useNewUrlParser: true,  useUnifiedTopology: true }
+ { useNewUrlParser: true,  useUnifiedTopology: true,  useCreateIndex: true }
 
  );
 mongoose.Promise = global.Promise;
